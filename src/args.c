9b5189dcb2c525d1dcc8d51b4d9b6754c91ac9e4
Francisco <fjgr.7.5.03@gmail.com>
2024-07-30 18:03:29 +0200

    Fixed typo

--- src/args.c
+++ src/args.c
@@ -3,12 +3,12 @@
 int	ft_check_args(int argc, char **argv)
 {
 	if (argc < 2 || argc > 6)
-		return (fprintf(stderr, "Usage: %s <send/recive>\n", argv[0]), 0);
-	if (!(strcmp(argv[1], "send") || strcmp(argv[1], "recive")))
-		return (fprintf(stderr, "Usage: %s <send/recive>\n", argv[0]), 0);
-	if (argc != 4 && strcmp(argv[1], "recive") == 0)
-		return (fprintf(stderr, "Usage: %s <send/recive> <port> <filename>\n", argv[0]), 0);
+		return (fprintf(stderr, "Usage: %s <send/recv>\n", argv[0]), 0);
+	if (!(strcmp(argv[1], "send") || strcmp(argv[1], "recv")))
+		return (fprintf(stderr, "Usage: %s <send/recv>\n", argv[0]), 0);
+	if (argc != 4 && strcmp(argv[1], "recv") == 0)
+		return (fprintf(stderr, "Usage: %s <send/recv> <port> <filename>\n", argv[0]), 0);
 	if (argc != 5 && strcmp(argv[1], "send") == 0)
-		return (fprintf(stderr, "Usage: %s <send/recive> <ip> <port> <file>\n", argv[0]), 0);
+		return (fprintf(stderr, "Usage: %s <send/recv> <ip> <port> <file>\n", argv[0]), 0);
 	return (1);
 }
